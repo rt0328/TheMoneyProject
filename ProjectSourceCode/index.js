a810d625a89f6d6b3ea9b6444ea5c585f45ff319
sko-grace <groh8854@colorado.edu>
2024-04-02 18:39:53 -0600

    Finish db and web containers working plus organize index.js such that the home page displays using the main layout (handlebars)

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -1,15 +1,40 @@
-const http = require('http');
+// ----------------------------------   DEPENDENCIES  ----------------------------------------------
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
+// const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
+// const session = require('express-session');
 
-const hostname = '0.0.0.0';
-const port = 3000;
+// -------------------------------------  APP CONFIG   ----------------------------------------------
 
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World\n');
+// Create `ExpressHandlebars` instance with the default layout and configure the layouts and partials dir.
+const hbs = handlebars.create({
+    extname: '.hbs', // Make sure you have the dot before 'hbs'
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, '/src/views/layouts'),
+    partialsDir: path.join(__dirname, '/src/views/partials'),
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/src/views'));
+app.use(bodyParser.json());
 
+// If you are using express-session, uncomment the session setup as well
+
+// -------------------------------------  ROUTES   ----------------------------------------------
+
+app.get('/', (req, res) => {
+    // Just provide the name of the view 'home'
+    // The .hbs extension is not required because you've set the default engine to handle it
+    res.render('pages/home');
+});
+
+// -------------------------------------  START THE SERVER   ----------------------------------------------
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
