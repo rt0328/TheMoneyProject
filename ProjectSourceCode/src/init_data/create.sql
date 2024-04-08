5b332e29c0dd4caf2f8bc150d8eb1ac91d4fae1c
Ryan Townsend <rtownsendmailbox@gmail.com>
2024-04-08 13:48:29 -0600

    fixed issue with db

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -2,24 +2,23 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(60) NOT NULL,
-    money FLOAT,
+    money FLOAT
 );
 
 DROP TABLE IF EXISTS stocks;
 
 CREATE TABLE stocks (
     id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
     name VARCHAR(100),
     symbol VARCHAR(10) NOT NULL,
-    price FLOAT,
+    price FLOAT
 );
 
 DROP TABLE IF EXISTS users_to_stocks;
 
 CREATE TABLE users_to_stocks(
-    user_id INT NOT NULL,
+    user_id VARCHAR(50) NOT NULL,
     stock_id INT NOT NULL,
     bought_price FLOAT,
-    qty INT,
+    qty INT
 );
