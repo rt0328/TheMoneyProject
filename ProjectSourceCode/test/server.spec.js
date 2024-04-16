ff0f7450d005b577a87d185347d41d40caaccf01
Ryan Townsend <rtownsendmailbox@gmail.com>
2024-04-16 16:40:55 -0600

    reinserted unit tests

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -10,77 +10,62 @@ chai.should();
 chai.use(chaiHttp);
 const { assert, expect } = chai;
 
-// ********************** DEFAULT WELCOME TESTCASE ****************************
 
-// describe('Server!', () => {
-//   // Sample test case given to test / endpoint.
-//   it('Returns the default welcome message', done => {
-//     chai
-//       .request(server)
-//       .get('/welcome')
-//       .end((err, res) => {
-//         expect(res).to.have.status(200);
-//         expect(res.body.status).to.equals('success');
-//         assert.strictEqual(res.body.message, 'Welcome!');
-//         done();
-//       });
-//   });
-// });
 
 // *********************** TODO: WRITE 2 UNIT TESTCASES **************************
 
 // ********************************************************************************
 
-// describe('Testing Add User API', () => {
-//   it('Positive : /register', done => {
-//     chai
-//       .request(server)
-//       .post('/register')
-//       .send({ username: 'testname', password: "password" })
-//       .end((err, res) => {
-//         expect(res).to.have.status(302);
-//         res.should.be.html;
-//         done();
-//       });
-//   });
+describe('Testing Add User API', () => {
+  it('Positive : /register', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({ username: 'testname', password: "password" })
+      .end((err, res) => {
+        expect(res).to.have.status(302);
+        res.should.be.html;
+        done();
+      });
+  });
 
-//   it('Negative : /register. Checking empty password', done => {
-//     chai
-//       .request(server)
-//       .post('/register')
-//       .send({ username: 'othername', password: "" }) //no password
-//       .end((err, res) => {
-//         expect(res).to.have.status(400);
-//         expect(res.text).to.include("Please enter a username and password");
+  it('Negative : /register. Checking empty password', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({ username: 'othername', password: "" }) //no password
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.text).to.include("Please enter a username and password");
 
-//         res.should.be.html;
-//         done();
-//       });
-//   });
-// });
+        res.should.be.html;
+        done();
+      });
+  });
+});
 
-// describe('Testing Login API', () => {
-//   it('Positive : /login', done => {
-//     chai
-//       .request(server)
-//       .post('/login')
-//       .send({ username: "exampleuser", password: "examplepassword" })
-//       .end((err, res) => {
-//         res.should.have.status(200);
-//         res.should.redirectTo(/^.*127\.0\.0\.1.*\/groups/);
-//         done();
-//       });
-//   });
-//   it('Negative : /login. Checking incorrect password', done => {
-//     chai
-//       .request(server)
-//       .post('/login')
-//       .send({ username: "exampleuser", password: "other" }) //incorrect password
-//       .end((err, res) => {
-//         res.should.have.status(400);
-//         res.should.be.html;
-//         done();
-//       });
-//   });
-// });
+describe('Testing Login API', () => {
+  it('Positive : /login', done => {
+    chai
+      .request(server)
+      .post('/login')
+      .send({ username: "exampleuser", password: "examplepassword" })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.redirectTo(/^.*127\.0\.0\.1.*\/groups/);
+        done();
+      });
+  });
+  it('Negative : /login. Checking incorrect password', done => {
+    chai
+      .request(server)
+      .post('/login')
+      .send({ username: "exampleuser", password: "other" }) //incorrect password
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.should.be.html;
+        done();
+      });
+  });
+});
 
